683e3d6e5b4d158c0f4b05b0a2004c4b3562d34a
Sanchez Mata, Alejandro <alejandro.sanchez.mata@everis.com>
2017-01-23 11:19:47 +0100

    tratamos la respuesta si el mensaje original es de fb

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -15,17 +15,24 @@ public class HelloWorldController {
     @RequestMapping(method = RequestMethod.POST)
     public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
 
+    	String response = "";
     	System.out.println("Entramos al metodo webhook");
         System.out.println(obj);
         
         System.out.println("---------------------");
         System.out.println("Parseamos el request");
         AIRequest request = new AIRequest(obj);
+        System.out.println(request);
         System.out.println("---------------------");
+        
         if(obj.contains("facebook")){
         	System.out.println("Peticion de facebook");
+        	response = "hola usuario de facebook";
+        }
+        else{
+        	response = "webhook Hello!";
         }
 
-        return new WebhookResponse("webhook Hello! ", "webhook Text");
+        return new WebhookResponse(response, "webhook Text");
     }
 }
