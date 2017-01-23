2ec426b7fd4dcc5cc113f71f4a6cf7ca471d27cc
Sanchez Mata, Alejandro <alejandro.sanchez.mata@everis.com>
2017-01-23 10:39:33 +0100

    Añadimos el parser del objeto de api.ai

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import ai.api.model.AIRequest;
+
 @Controller
 @RequestMapping("/webhook")
 public class HelloWorldController {
@@ -16,7 +18,14 @@ public class HelloWorldController {
     	System.out.println("Entramos al metodo webhook");
         System.out.println(obj);
         
+        System.out.println("---------------------");
+        System.out.println("Parseamos el request");
+        AIRequest request = new AIRequest(obj);
+        System.out.println("---------------------");
+        if(obj.contains("facebook")){
+        	System.out.println("Peticion de facebook");
+        }
 
-        return new WebhookResponse("Hello! " + obj, "Text " + obj);
+        return new WebhookResponse("webhook Hello! ", "webhook Text");
     }
 }
