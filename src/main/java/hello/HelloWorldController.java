d46d3fe8c29a6bf544b0736412b44d80fe409133
Sanchez Mata, Alejandro <alejandro.sanchez.mata@everis.com>
2017-01-24 12:25:41 +0100

    parseamos la query

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import com.restfb.DefaultJsonMapper;
 import com.restfb.JsonMapper;
 import com.restfb.types.webhook.WebhookObject;
 
+import ai.api.model.AIContext;
 import ai.api.model.AIRequest;
 import ai.api.model.Entity;
 
@@ -17,38 +20,47 @@ import ai.api.model.Entity;
 @RequestMapping("/webhook")
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.POST)
-    public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
+	@RequestMapping(method = RequestMethod.POST)
+	public @ResponseBody WebhookResponse webhook(@RequestBody String obj) {
 
-    	String response = "";
-    	System.out.println("Entramos al metodo webhook");
-        System.out.println(obj);
-        
-        System.out.println("---------------------");
-        System.out.println("Parseamos el request");
-        AIRequest request = new AIRequest(obj);
-        System.out.println(request);
-        System.out.println("---------------------");
-        
-        if(obj.contains("facebook")){
-        	System.out.println("Peticion de facebook");
-        	response = "hola usuario de facebook";
-        	System.out.println("entidades: ");
-        	System.out.println("***");
-        	for (Entity entidad: request.getEntities()){
-        		System.out.println(entidad);
-        		System.out.println("***");
-        	}
-        	System.out.println("***");
-        	request.getEntities();
-        	JsonMapper mapper = new DefaultJsonMapper();
-        	WebhookObject webhookObject = mapper.toJavaObject(obj.toString(), WebhookObject.class);
-        	System.out.println(webhookObject.toString());
-        }
-        else{
-        	response = "webhook Hello!";
-        }
+		String response = "";
+		System.out.println("Entramos al metodo webhook");
+		System.out.println(obj);
 
-        return new WebhookResponse(response, "webhook Text");
-    }
+		System.out.println("---------------------");
+		System.out.println("Parseamos el request");
+		AIRequest request = new AIRequest(obj);
+		System.out.println(request);
+		System.out.println("---------------------");
+
+		if (obj.contains("facebook")) {
+			System.out.println("Peticion de facebook");
+			response = "hola usuario de facebook";
+			System.out.println("entidades: ");
+			System.out.println("***");
+			if (null != request.getEntities()) {
+				for (Entity entidad : request.getEntities()) {
+					System.out.println(entidad);
+					System.out.println("***");
+				}
+			} else {
+				System.out.println("No hay entidades");
+			}
+			System.out.println("***");
+			
+			System.out.println("Recuperamos los datos de la query");
+			String[] query = new String[]{obj};
+			System.out.println(query[3]);
+			//List<AIContext> contexts = query[3];
+
+			JsonMapper mapper = new DefaultJsonMapper();
+			WebhookObject webhookObject = mapper.toJavaObject(obj.toString(),
+					WebhookObject.class);
+			System.out.println(webhookObject.toString());
+		} else {
+			response = "webhook Hello!";
+		}
+
+		return new WebhookResponse(response, "webhook Text");
+	}
 }
